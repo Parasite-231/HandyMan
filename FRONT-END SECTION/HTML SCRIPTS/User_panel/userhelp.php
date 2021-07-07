7395e95bf1c07c22841dae62688e878c3129e340
Ifty <iftikharimrul@iut-dhaka.edu>
2021-07-07 12:07:41 +0600

    userhelp

--- FRONT-END SECTION/HTML SCRIPTS/User_panel/userhelp.php
+++ FRONT-END SECTION/HTML SCRIPTS/User_panel/userhelp.php
@@ -46,48 +46,54 @@
                     <h2>FAQ</h2>
                 </div>
                 <div class="content">
-                <h3>Q. How does this site work?</h3>
-                  <p> 
-                     You can browse the site or use our search engine to find your desired service. You can then add them to your my orders platform and click on confirm order. You have to let us know your address, select a service time – you are done. An worker for that particular service will 
-                     arrive at your place with his helping hand. 
-                  </p>
-                  <h3>Q. Can I login to this website using phone number?</h3>
-                  <p>Yes, you can login to these website by using both phone number and username after signing up</p>
+                    <h3>Q. How does this site work?</h3>
+                    <p>
+                        You can browse the site or use our search engine to find your desired service. You can then add
+                        them to your my orders platform and click on confirm order. You have to let us know your
+                        address, select a service time – you are done. An worker for that particular service will
+                        arrive at your place with his helping hand.
+                    </p>
+                    <h3>Q. Can I login to this website using phone number?</h3>
+                    <p>Yes, you can login to these website by using both phone number or email address after signing up
+                    </p>
 
 
-                  <h3>Q. How can I contact you?</h3>
-                  <p>You can always contact us through our contact numbers provided in our contact us page and can also mail us at <a href="mdmuktadirmazumder@gmail.com">reachus@handyman.com</a></a><br>
-                  Visit : <a href="contactUs.html">www.handyman.com</a><br>
-                  Contact: +8801306989578 </p>
+                    <h3>Q. How can I contact you?</h3>
+                    <p>You can always contact us through our contact numbers provided in our contact us page and can
+                        also mail us at <a href="mdmuktadirmazumder@gmail.com">reachus@handyman.com</a></a><br>
+                        Visit : <a href="contactUs.php">www.handyman.com</a><br>
+                        Contact: +8801306989578 </p>
 
-                  <h3>Q. How do I know when my order is here? </h3>
-                  <p> The employee you will select for your particular work will call you as soon as they are at your house. </p>
+                    <h3>Q. How do I know when my order is here? </h3>
+                    <p> The employee you will select for your particular work will call you as soon as they are at your
+                        house. </p>
 
-                  <h3>Q. What are your servicing hours? </h3>
-                  <p>We deliever our services from 8 am to 10 pm every day. You can also make your reserve time for your work.</p>
+                    <h3>Q. What are your servicing hours? </h3>
+                    <p>We deliver our services from 8 am to 10 pm every day. You can also make your reserve time for
+                        your work.</p>
 
-                  <h3>Q. Can I cancel my order?</h3>
-                  <p>No, you can't cancel your oder </p>
+                    <h3>Q. Can I cancel my order?</h3>
+                    <p>No, you can't cancel your order </p>
 
-                  <h3>Q. How do I pay? </h3>
-                  <p>We accept cash on delivery only. </p>
-                  
-                 
-                  <h3>Q. What happens during a hartal? </h3>
-                  <p>We work during hartals. That’s how dedicated we are. </p>
-                  
-                  <h3>Q. Do you serve my area? </h3>
-                  <p>We are currently serving all of Dhaka city, except certain parts of Old town. </p>
-                  
-                 
-                  <h3>Q.  Can I order over the phone? </h3>
-                  <p>No, You have to order your work from our website only</p>
-                 
-                  <h3>Q. How are you sourcing your employees? </h3>
-                  <p>Any worker who are experienced and well-skilled are only being choose from this company. </p>
+                    <h3>Q. How do I pay? </h3>
+                    <p>We accept cash on delivery only. </p>
 
-                  <h3>Q. Should I tip the worker ? </h3>
-                  <p>Tips are not required. But our employees appreciate recognition for their hard work.</p>
+
+                    <h3>Q. What happens during a strike? </h3>
+                    <p>We work during strikes. That’s how dedicated we are. </p>
+
+                    <h3>Q. Do you serve my area? </h3>
+                    <p>We are currently serving all of Dhaka city, except certain parts of Old town. </p>
+
+
+                    <h3>Q. Can I order over the phone? </h3>
+                    <p>No, You have to order your work from our website only</p>
+
+                    <h3>Q. How are you sourcing your employees? </h3>
+                    <p>Any worker who are experienced and well-skilled are only being choose from this company. </p>
+
+                    <h3>Q. Should I tip the worker ? </h3>
+                    <p>Tips are not required. But our employees appreciate recognition for their hard work.</p>
                     <!--<div class="button">
                      <a href="">Connect with us</a>
                      </div>-->
@@ -100,7 +106,7 @@
                   </div>-->
             </div>
             <div class="img-section">
-                <img src="policyposter4.png">
+                <img src="../../ICONS/policyposter4.png">
             </div>
         </div>
     </div>
