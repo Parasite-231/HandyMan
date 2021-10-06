2e0135a6f6cf6ce916fe7580ecd5dc08f2d7a7bd
Ifty <iftikharimrul@iut-dhaka.edu>
2021-10-06 19:02:28 +0600

    fixed bug

--- FRONT-END SECTION/HTML SCRIPTS/Admin_panel/ComplaintList.php
+++ FRONT-END SECTION/HTML SCRIPTS/Admin_panel/ComplaintList.php
@@ -73,9 +73,9 @@ if (!isset($_SESSION['aID'])) {
             <!--send message link-->
 
             <li class="log_out">
-                <a href="../../Inc/admin_logout.php"></a>
-                <i class='bx bx-log-out'></i>
-                <span class="links_name">Log out</span>
+                <a href="../../Inc/admin_logout.php">
+                    <i class='bx bx-log-out'></i>
+                    <span class="links_name">Log out</span>
                 </a>
             </li>
         </ul>
