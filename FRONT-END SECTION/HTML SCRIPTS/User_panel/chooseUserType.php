3405b7cce8e3ad40f0551e7838042b5dc70aab2a
Parasite-231 <muktadir@iut-dhaka.edu>
2022-01-25 10:42:19 +0600

    User/ChooseUserType 2.0

--- FRONT-END SECTION/HTML SCRIPTS/User_panel/chooseUserType.php
+++ FRONT-END SECTION/HTML SCRIPTS/User_panel/chooseUserType.php
@@ -7,7 +7,6 @@ if (isset($_SESSION['uID'])) {
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,6 +16,7 @@ if (isset($_SESSION['uID'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/x-icon" href="../../ICONS/worker.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../../CSS SCRIPTS/user_panel/chooseUserTypeDesign.css">
     <title>Choose Platform</title>
 </head>
@@ -27,22 +27,27 @@ if (isset($_SESSION['uID'])) {
             <div class="company-logo">
                 <!--<img src="hANDYman.png">-->
             </div>
-            <!--<ul>
-                    <li class="active-mode" ><a href="#">Home</a> </li>
-                    <li><a href="#">Abou us</a> </li>
-                    <li><a href="#">Contact us</a> </li>
-                    <li><a href="#">Help</a> </li>
-                    
-                    </ul>-->
+           
         </div>
+        <!--Changed 2.0-->
         <div class="platform-selection">
             <h2>Platform Selection</h2>
+            <a href="userSignup.php"><button type="button" class="btn btn-dark btn-lg">Customer Platform</button></a>
+            <a href="../Employee_panel/employee registration.php"><button type="button" class="btn btn-dark btn-lg">Employee Platform</button></a>
         </div>
+        <!--Changed 2.0-->
+
+        
+
+        <!-- <button type="button" class="btn btn-dark btn-lg">Customer Platform</button>
+        <button type="button" class="btn btn-dark btn-lg">Employee Platform</button> -->
+      
         <!--platform choosing option-->
-        <div class="button">
+        <!-- <div class="button">
             <a href="userSignup.php" class="btn">Customer Platform</a>
             <a href="../Employee_panel/employee registration.php" class="btn">Employee Platform</a>
-        </div>
+        </div> -->
+        
     </header>
 </body>
 
