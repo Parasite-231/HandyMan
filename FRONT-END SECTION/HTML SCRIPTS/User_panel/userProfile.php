bea3a8670721f74be3278bcd4c8d5113313ba519
Parasite-231 <muktadir@iut-dhaka.edu>
2022-01-26 13:53:50 +0600

    UserProfile

--- FRONT-END SECTION/HTML SCRIPTS/User_panel/userProfile.php
+++ FRONT-END SECTION/HTML SCRIPTS/User_panel/userProfile.php
@@ -31,105 +31,55 @@ if (isset($_POST['submit'])) {
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>My Profile</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    	<link rel="shortcut icon" type="image/x-icon" href="../../ICONS/worker.png">
+    	<link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="../../CSS SCRIPTS/user_panel/UrProfileDesign.css">
+	</head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../CSS SCRIPTS/user_panel/userProfile.css">
-    <link rel="shortcut icon" type="image/x-icon" href="../../ICONS/worker.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
-    <title>Profile</title>
-</head>
 
-<body>
-    <div class="bg-img">
-        <div class="container">
-            <div class="topnav">
-                <a href="userProfile.php"><i class="fa  fa-user"></i>Profile</a>
-                <a href="index.php"><i class="fa  fa-house-user"></i>Home</a>
-                <a href="userOrderHistoryPage.php"><i class="fa  fa-shopping-cart"></i>My Orders</a>
-                <a href="About us.php" class="constant"><i class="fa  fa-users"></i>About Us</a>
-                <a href="contactUs.php"><i class="fa fa-phone-square"></i>Contact Us</a>
-                <a href="userTerms&policy.php" class="constant"><i class="fa fa-file-signature"></i>Terms & Policy</a>
-                <a href="userhelp.php"><i class="fa fa-question-circle"></i>Help</a>
-                <a href='../../Inc/logout.php'><i class='fa fa-lock'></i>Sign out</a>
-            </div>
-        </div>
-    </div>
-    <div class="section">
-        <div class="container-2">
-            <div class="content-section">
-                <div class="title">
-                    <h2>Profile</h2>
-                    <!-- <div class="shadowimg">
-                        <img src="../../UI-UX Designs/elonmusk.jpg">
-                        <div class="container-4">
-                            
-                        </div>
-                    </div> -->
-                </div>
-                <div class="content">
-                    <!--profile-form-->
-                    <div class="container-3">
-                        <form method="POST">
-                            <div class="heading-of-profile-card">
-                                <h2>Profile Card</h2>
-                            </div>
-                            <div class="row">
-                                <div class="colsection1">
-                                    <label for="name">Name</label>
-                                </div>
-                                <div class="colsection2">
-                                    <input type="text" id="name" name="name" value="<?php echo $name; ?>">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="colsection1">
-                                    <label for="email">Email</label>
-                                </div>
-                                <div class="colsection2">
-                                    <input type="text" id="email" name="email" value="<?php echo $email; ?>">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="colsection1">
-                                    <label for="phone-number">Mobile number</label>
-                                </div>
-                                <div class="colsection2">
-                                    <input type="text" id="email" name="number" value="<?php echo $number; ?>">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="colsection1">
-                                    <label for="address">Address</label>
-                                </div>
-                                <div class="colsection2">
-                                    <textarea id="address" name="address"
-                                        style="height:100px; resize:none"><?php echo $address; ?></textarea>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <input type="submit" name="submit" value="Submit">
-                            </div>
-                        </form>
-                    </div>
-                    <!--profile-form-->
-                    <!--<div class="button">
-                     <a href="">Connect with us</a>
-                     </div>-->
-                </div>
-                <!--<div class="social-media">
-                  <a href=""><i class="fab fa-facebook"></i></a>
-                  <a href=""><i class="fab fa-linkedin"></i></a>
-                  <a href=""><i class="fab fa-instagram-square"></i></a>
-                  <a href=""><i class="fab fa-twitter-square"></i></a>
-                  </div>-->
-            </div>
-        </div>
-    </div>
-</body>
+	<body>
+		<div class="main">
 
+		<div class="wrapper">
+			<div class="inner">
+				<div class="image-holder">
+					<img src="../../UI-UX Designs/b000.webp" height="500px" width="600px" alt="">
+				</div>
+				<form method="POST">
+					<h3 style="font-size: 30px;color: brown;">My Profile </h3>
+					<div class="form-row">
+						<input type="text" id="name" name="name"  class="form-control" placeholder="Enter your Name"  value="<?php echo $name; ?>">
+						<input type="text" id="email" name="email" class="form-control" placeholder="Enter your Mail" value="<?php echo $email; ?>">
+					</div>
+					<div class="form-row">
+						<input type="text" id="email" name="number" class="form-control"  placeholder="Mobile Number" value="<?php echo $number; ?>">
+						
+					</div>
+					<textarea  id="address" name="address" placeholder="Your Present Address"  class="form-control"  style="height: 130px;resize: none;" ><?php echo $address; ?></textarea>
+				
+						<div class="form-row">
+							<button type="submit" name="submit" value="Submit">Submit
+								<!-- <i class="zmdi zmdi-long-arrow-right"></i> -->
+							</button>
+							<a href="index.php" style="color: white;"> <button type="submit" name="submit" value="Submit">Go Back</a>
+								<!-- <i class="zmdi zmdi-long-arrow-right"></i> -->
+							</button>
+							
+						</div>
+				
+				
+					
+				</form>
+				
+			</div>
+		</div>
+	</div>
+
+	</body>
 </html>
