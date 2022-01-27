80ae7218bdacf24a4397b70544a325223ab37793
Parasite-231 <muktadir@iut-dhaka.edu>
2022-01-27 14:51:09 +0600

    UserCovPage

---
+++ FRONT-END SECTION/HTML SCRIPTS/User_panel/UserCoverPage.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href="../../CSS SCRIPTS/user_panel/CovDes.css">
+        <link rel="shortcut icon" type="image/x-icon" href="../../ICONS/worker.png">
+        <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Welcome</title>
+    </head>
+    <body class="d-flex h-100 text-center  bg-image bg-dark" style="color: white;">
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <header class="mb-auto">
+                <div>
+                    <h3 class="float-md-start mb-0"></h3>
+                    <nav class="nav nav-masthead justify-content-center float-md-end">
+                        <a class="nav-link active" aria-current="page" style="color: black;" href="UserCoverPage.php">Welcome</a>
+                        <a class="nav-link" style="color: black" href="index.php">&nbsp;&nbsp;&nbsp;Home</a>
+                        <a class="nav-link" style="color: black" href="About us.php">&nbsp;&nbsp;&nbsp;About</a>
+                        <a class="nav-link" style="color: black" href="contactUs.php">&nbsp;&nbsp;&nbsp;Contact</a>
+                        <a class="nav-link" style="color: black" href="userTerms&policy.php">&nbsp;&nbsp;&nbsp;Policy</a>
+                        <a class="nav-link" style="color: black" href="userhelp.php">&nbsp;&nbsp;&nbsp;Help</a>
+                    </nav>
+                </div>
+            </header>
+            <main class="px-3">
+                <h1 style="color: rgb(255, 217, 0);font-weight: 700;">HandyMan</h1>
+                <p class="lead">
+                    We provide 24/7 online support, subsidized customer service. Your details will never be shared with a third party and your anonymity is important to us.
+                </p>
+                <p class="lead">
+                    
+                    <!-- <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white" type="button">Learn more</a> -->
+                    <a href="userLogin.php"><button type="button"  class="btn btn-dark btn-lg" >Sign-in</button></a>
+                    <a href="user signup.php"><button type="button" class="btn btn-light btn-lg">Sign-up</button></a>
+                </p>
+            </main>
+            <footer class="mt-auto text-white-50">
+              <!--future use-->
+            </footer>
+        </div>
+    </body>
+</html>
